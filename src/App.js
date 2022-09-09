033f0639801791f58b93f37e079ebaff7f0e1acf
Michał Markiewicz <michalmarkiewicz0953@gmail.com>
2022-09-09 15:25:31 +0200

    calculateAvg()

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App()
 {
-  
-  const [savedDays, setSavedDays] = useState([]);
+  const savedDaysFromLocalStorage = JSON.parse(localStorage.getItem("savedDays"));
+  const [savedDays, setSavedDays] = useState(savedDaysFromLocalStorage === null ? [] : savedDaysFromLocalStorage);
 
   return (
     <div id="app">
@@ -31,8 +31,17 @@ function UserInput({savedDays, setSavedDays})
       wakeTime: wakeTime.value
     }
 
-    savedDays.push(day);
-    setSavedDays(savedDays);
+    const newSavedDays = savedDays;
+    newSavedDays.push(day);
+
+    setSavedDays(newSavedDays);
+    saveToLocalStorage(newSavedDays);
+  }
+
+  function saveToLocalStorage(savedDaysArray)
+  {
+    const savedDaysArrayJson = JSON.stringify(savedDaysArray);
+    localStorage.setItem("savedDays", savedDaysArrayJson);
   }
 
   return (
@@ -43,17 +52,17 @@ function UserInput({savedDays, setSavedDays})
       </div>
 
       <div className="user-input-container">
-        <label htmlFor="user-input-relax-time">Relax time</label>
+        <label htmlFor="user-input-relax-time">Relax time (h:m)</label>
         <input type="text" id="user-input-relax-time"></input>
       </div>
 
       <div className="user-input-container"> 
-        <label htmlFor="user-input-sleep-time">Sleep time</label>
+        <label htmlFor="user-input-sleep-time">Sleep time (h:m)</label>
         <input type="text" id="user-input-sleep-time"></input>
       </div>
 
       <div className="user-input-container">
-        <label htmlFor="user-input-wake-time">Wake up time</label>
+        <label htmlFor="user-input-wake-time">Wake up time (h:m)</label>
         <input type="text" id="user-input-wake-time"></input>
       </div>
 
@@ -104,5 +113,38 @@ function AvgWakeTime()
     </div>
   );
 }
+
+function calculateAvg(hoursMinutesArray)
+{
+  // array format: ["10:20", "14:50", "06:20"];
+
+  // 1. Get data, split hours and minutes 
+  // 2. Sum minutes ((hours / 60) + minutes)
+  // 3. Divide by number of days 
+  // 4. Avg in minutes * 60 
+  // 5. Use Math.round on remainder
+  // 6. Add it to hours
+  // 7. Return string value of result 
+
+  let hours = 0;
+  let minutes = 0;
+
+  hoursMinutesArray.forEach((entry) => {
+    const hoursMinutesSplit = entry.split(":");
+    hours += Number(hoursMinutesSplit[0]);
+    minutes += Number(hoursMinutesSplit[1]);
+  })
+
+  const sumOfMinutes = (hours * 60) + minutes;
+  const avgFormula = sumOfMinutes / hoursMinutesArray.length;
+  const convertToHours = avgFormula / 60;
+  const getMinutes = Math.round((convertToHours % 1) * 60);
+  const getHours = convertToHours - (convertToHours % 1);
+  const result = `${getHours}:${getMinutes}`;
+  
+  return result;
+}
+
+console.log(calculateAvg(["10:20", "20:10", "19:13", "12:19", "06:20"]));
 
 export default App;
